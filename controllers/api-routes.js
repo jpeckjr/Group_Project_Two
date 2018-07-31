f77725a5c724d000f1d13d5f80f8444c5fd4a869
NDKivi <kivineal@outlook.com>
2018-07-31 12:35:51 -0500

    users model and controller

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,24 +1,44 @@
 const db = require("../models");
 
-module.exports = function(app, cb) {
+module.exports = function (app, cb) {
 
-    app.post("/api/register", function(req, res) {
-        res.json({"success":"success"});
+    app.post("/api/register", function (req, res) {
+        res.json({ "success": "success" });
         console.log(req);
     });
 
-    app.post("/api/login", function(req, res) {
-        res.json({"success":"success"});
-        console.log(req);
+    app.post("/api/login", function (req, res) {
+        let token = "";
+        let data = "";
+        let error = "";
+        db.User.findOne({
+            where: {
+                username: req.body.username.toLowerCase()
+            }
+        }).then(function (user) {
+            let userObject = user.dataValues;
+            console.log(userObject);
+            if (userObject) {
+                if (req.body.password === userObject.password) {
+                    token = "nvnvnvnvnvnxcqwerqwerqwer";
+                } else {
+                    error = "Invalid password";
+                }
+            } else {
+                error = "Invalid username";
+            }
+            res.json({"data": data, "token": token, "error": error});
+        });
+        
     });
-    
-    app.get("/api/search", function(req, res) {
-        res.json({"success":"success"});
+
+    app.get("/api/search", function (req, res) {
+        res.json({ "success": "success" });
         console.log(req);
     });
 
-    app.post("/api/save", function(req, res) {
-        res.json({"success":"success"});
+    app.post("/api/save", function (req, res) {
+        res.json({ "success": "success" });
         console.log(req);
     });
 
--- models/user.js
+++ models/user.js
@@ -1,17 +1,30 @@
 module.exports = function (sequelize, DataTypes) {
     //do sequelize define for the users table
-    let Users = sequelize.define("UsersTest", {
-        something: {
+    let Users = sequelize.define("User", {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            unique: true,
+            autoIncrement: true
+        },
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [1]
+                isAlphanumeric: true
             }
         },
-        body: {
-            type: DataTypes.TEXT,
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
             allowNull: false,
-            len: [1]
+            validate: {
+                isEmail: true
+            }
         }
     });
 
